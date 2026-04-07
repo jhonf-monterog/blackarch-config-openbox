5874eacb510ed85d87f295c90e190665f4c577d3
Jhon Montero <jhonf.monterog@gmail.com>
2026-04-06 23:29:50 -0500

    Remove package installation and system upgrade commands

--- install.sh
+++ install.sh
@@ -1,29 +1,4 @@
 #!/usr/bin/env bash
 
-# ----------------------------------------------------------------------------------------------------
-# Sync package databases and upgrade system
-# ----------------------------------------------------------------------------------------------------
-
-sudo pacman --sync --refresh --sysupgrade --noconfirm
-
-# ----------------------------------------------------------------------------------------------------
-# Install required packages
-# ----------------------------------------------------------------------------------------------------
-
-sudo pacman --sync --needed --noconfirm feh
-sudo pacman --sync --needed --noconfirm openbox
-sudo pacman --sync --needed --noconfirm python
-sudo pacman --sync --needed --noconfirm python-pyxdg
-sudo pacman --sync --needed --noconfirm rxvt-unicode
-sudo pacman --sync --needed --noconfirm terminus-font
-sudo pacman --sync --needed --noconfirm xorg-server
-sudo pacman --sync --needed --noconfirm xorg-xinit
-
-# ----------------------------------------------------------------------------------------------------
-# Copy configuration files
-# ----------------------------------------------------------------------------------------------------
-
 cp -rfv home/user/. ~/
 sudo cp -rfv usr/share/. /usr/share
-
-# ----------------------------------------------------------------------------------------------------
